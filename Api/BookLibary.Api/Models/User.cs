150e61f4fa20d169cac780cb50da6a965287e87b
esmanurgokkaya <esmanurgkky10@gmail.com>
2024-08-08 09:44:07 +0300

    adding gender and profile

--- Api/BookLibary.Api/Models/User.cs
+++ Api/BookLibary.Api/Models/User.cs
@@ -3,6 +3,12 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace BookLibary.Api.Models
 {
+
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
     public class User:IUser
     {
         [BsonId]
@@ -26,6 +32,12 @@ namespace BookLibary.Api.Models
 
         [BsonElement("Admin")]
         public bool IsAdmin { get; set; }
+
+        [BsonElement("AvatarUrl")]
+        public string avatarUrl { get; set; }
+
+        [BsonElement("Gender")]
+        public GenderType gender {  get; set; }
 
 
     }
