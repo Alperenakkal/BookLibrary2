d596298e9ba0d000994b9720dab2d224658dcbb1
Alperenakkal <alperenakkal06@gmail.com>
2024-08-09 17:06:21 +0300

    emailverifyfix

--- Api/BookLibary.Api/Models/Email.cs
+++ Api/BookLibary.Api/Models/Email.cs
@@ -2,11 +2,11 @@
 {
     public class Email
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string EmailAddress { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
-        public string Message { get; set; }
-        public string HtmlContent { get; set; }
+        public string? Message { get; set; }
+        public string? HtmlContent { get; set; }
     }
 }
