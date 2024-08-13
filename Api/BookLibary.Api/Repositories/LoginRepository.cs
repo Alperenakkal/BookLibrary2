1815ee46a2bc9102e976fb58f4a94be3ee2076e2
Alperenakkal <alperenakkal06@gmail.com>
2024-08-13 10:07:01 +0300

    emailfetchingcontroller

--- Api/BookLibary.Api/Repositories/LoginRepository.cs
+++ Api/BookLibary.Api/Repositories/LoginRepository.cs
@@ -4,6 +4,7 @@ using BookLibary.Api.Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace BookLibary.Api.Repositories
     public class LoginRepository : IUserRepository<User>
     {
         private readonly MongoDbContext _context;
-        private readonly IMongoCollection<User> _model;  
+        private readonly IMongoCollection<User> _model;
 
         public LoginRepository(MongoDbContext context)
         {
@@ -20,12 +21,34 @@ namespace BookLibary.Api.Repositories
             _model = context.GetCollection<User>("Users");
         }
 
-       
+
 
         public async Task<User> GetByNameAsync(string name)
         {
-            var filter = Builders<User>.Filter.Eq(x => x.UserName, name);
-            return await _model.Find(filter).FirstOrDefaultAsync();
+            try
+            {
+                var filter = Builders<User>.Filter.Eq(x => x.UserName, name);
+                return await _model.Find(filter).FirstOrDefaultAsync();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("User Fetch operation failed");
+            }
+            
+        }
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            try
+            {
+                var filter = Builders<User>.Filter.Eq(x => x.Email, email);
+                return await _model.Find(filter).FirstOrDefaultAsync();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("User filter email fetch operation failed");
+            }
         }
 
         public async Task<User> GetUserById(object _id)
@@ -38,7 +61,7 @@ namespace BookLibary.Api.Repositories
             catch (Exception)
             {
 
-                throw new Exception("Id getirme işlemi başarısız");
+                throw new Exception("Id fetching operation failed");
             }
         }
 
@@ -50,14 +73,14 @@ namespace BookLibary.Api.Repositories
             {
                 var filter = Builders<User>.Filter.Eq("_id", new ObjectId(id.ToString()));
                 var update = Builders<User>.Update
-          .Set(u => u.FullName, entity.FullName)
-          .Set(u => u.UserName, entity.UserName)
-          .Set(u => u.Email, entity.Email)
+            .Set(u => u.FullName, entity.FullName)
+            .Set(u => u.UserName, entity.UserName)
+            .Set(u => u.Email, entity.Email)
             .Set(u => u.gender, entity.gender)
-           .Set(u => u.avatarUrl, entity.avatarUrl)
-           .Set(u => u.ReadOutBooks, entity.ReadOutBooks)
-           .Set(u => u.BorrowBooks, entity.BorrowBooks);
-      
+            .Set(u => u.avatarUrl, entity.avatarUrl)
+            .Set(u => u.ReadOutBooks, entity.ReadOutBooks)
+            .Set(u => u.BorrowBooks, entity.BorrowBooks);
+
 
 
                 var result = await _model.UpdateOneAsync(filter, update);
@@ -92,7 +115,7 @@ namespace BookLibary.Api.Repositories
 
                 if (result.IsAcknowledged && result.ModifiedCount > 0)
                 {
-                    // Kitap başarıyla silindi, güncellenmiş kullanıcıyı döndür
+                 
                     return await _model.Find(filter).FirstOrDefaultAsync();
                 }
                 else
