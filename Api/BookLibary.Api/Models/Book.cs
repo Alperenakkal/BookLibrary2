c67afd248595dd944c5dad7d64df0d3c0eb673c7
Haluk Aytış <halukaytis@hotmail.com>
2024-08-19 10:58:40 +0300

    IsAvailable Update

--- Api/BookLibary.Api/Models/Book.cs
+++ Api/BookLibary.Api/Models/Book.cs
@@ -2,30 +2,29 @@
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace BookLibary.Api.Models
- {
-     public class Book:IBook
-     {
-         [BsonId]
-         public ObjectId Id { get; set; }
+{
+    public class Book : IBook
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
 
-         [BsonElement("BookName")]
-         public  string BookName { get; set; } = string.Empty;
-         [BsonElement("Publisher")]
+        [BsonElement("BookName")]
+        public string BookName { get; set; } = string.Empty;
 
-         public string? Publisher {get; set;}
-         [BsonElement("Author")]
+        [BsonElement("Publisher")]
+        public string? Publisher { get; set; }
 
-         public string? Author { get; set; }
-         [BsonElement("IsAvailable")]
+        [BsonElement("Author")]
+        public string? Author { get; set; }
 
-         public bool IsAvailable { get; set; }
-        [BsonElement("Stock")]
-        public int Stock { get; set; }
         [BsonElement("CoverImageUrl")]
         public string CoverImageUrl { get; set; }
+
         [BsonElement("Category")]
-        public string Category { get; set; }    
-
-
-     }
- }
+        public string Category { get; set; }
+        [BsonElement("Stock")]
+        public int Stock { get; set; }
+        [BsonElement("IsAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
